a9565ab260e2da572145dfb61dcb5ad02928877f
baaash <bleat@baaa.sh>
2023-03-23 19:29:40 +1300

    Added "Before Installing" section detailing required fonts, and logo

--- melancholy-theme.el
+++ melancholy-theme.el
@@ -21,7 +21,6 @@
 ;; Ensure the following fonts are installed before use:
 ;;
 ;;    - Impact_Label (https://www.fontsquirrel.com/fonts/download/impact-label)
-;;    - texlive-fonts-extra
 ;;    - texlive-sourcecodepro.
 ;;    - fonts-purisa || thai-scalable-purisa-fonts
 ;;    - fonts-dancingscript
